e043f37bc9c9a48e77626e6795a3023ad0a8a02b
jerryxihe <46834951+jerryxihe@users.noreply.github.com>
2020-01-11 14:12:18 -0500

    Added matrix generation and printing

--- solution.cpp
+++ solution.cpp
@@ -6,27 +6,6 @@
 
 using namespace std;
 
-int minDistance(string word1, string word2) {
-    vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1));
-    dp[0][0] = 0;
-    for(int i = 0; i < word1.size(); ++ i)
-        dp[i+1][0] = i+1;
-    for(int i = 0; i < word2.size(); ++ i)
-        dp[0][i+1] = i+1;
-    
-    for(int i = 0; i < word1.size(); ++ i){
-        for(int j = 0; j < word2.size(); ++ j){
-            if(word1[i] == word2[j]){
-                dp[i+1][j+1] = dp[i][j];
-            }
-            else{
-                dp[i+1][j+1] = min(dp[i][j], min(dp[i][j+1], dp[i+1][j])) + 1;
-            }
-        }
-    }
-    return dp.back().back();
-}
-
 int main(int argc, char* argv[]) {
     string lineToAdd;
     string firstLine;
@@ -53,16 +32,50 @@ int main(int argc, char* argv[]) {
     while (getline(f, lineToAdd)) {
         desiredOrder.push_back(lineToAdd);
     }
-
-    cout << "Original vector\n";
-    for (unsigned int i = 0; i < originalOrder.size(); ++i) {
-        cout << i << ' ' << originalOrder[i] << '\n';
-    }
-    cout << '\n';
-    cout << "Desired vector\n";
-    for (unsigned int i = 0; i < desiredOrder.size(); ++i) {
-        cout << i << ' ' << desiredOrder[i] << '\n';
-    }
-    cout << '\n';
     f.close();
+
+    // cout << "Original vector\n";
+    // for (unsigned int i = 0; i < originalOrder.size(); ++i) {
+    //     cout << i << ' ' << originalOrder[i] << '\n';
+    // }
+    // cout << '\n';
+    // cout << "Desired vector\n";
+    // for (unsigned int i = 0; i < desiredOrder.size(); ++i) {
+    //     cout << i << ' ' << desiredOrder[i] << '\n';
+    // }
+    // cout << '\n';
+
+    vector<vector<int>> dp(originalOrder.size()+1, vector<int>(desiredOrder.size()+1));
+    dp[0][0] = 0;
+    for(int i = 0; i < originalOrder.size(); ++ i)
+        dp[i+1][0] = i+1;
+    for(int i = 0; i < desiredOrder.size(); ++ i)
+        dp[0][i+1] = i+1;
+        
+    for(int i = 0; i < originalOrder.size(); ++ i){
+        for(int j = 0; j < desiredOrder.size(); ++ j){
+            if(originalOrder[i] == desiredOrder[j]){
+                dp[i+1][j+1] = dp[i][j];
+            }
+            else{
+                dp[i+1][j+1] = min(dp[i][j], min(dp[i][j+1], dp[i+1][j])) + 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < originalOrder.size()+1; ++i) {
+        for (int j = 0; j < desiredOrder.size()+1; ++j) {
+            if (dp[i][j] < 10) {
+                cout << ' ';
+            }
+            if (dp[i][j] < 100) {
+                cout << ' ';
+            }
+            if (dp[i][j] < 1000) {
+                cout << ' ';
+            }
+            cout << dp[i][j] << ' ';
+        }
+        cout << '\n';
+    }
 }
