da0fddb2eb441c808a04448bfa90f91e38e78a95
1999michael <michael.ruan@mail.utoronto.ca>
2020-01-11 13:58:26 -0500

    added librarian problem checking 2 strings, test

--- solution.cpp
+++ solution.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+int minDistance(string word1, string word2) {
+    vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1));
+    dp[0][0] = 0;
+    for(int i = 0; i < word1.size(); ++ i)
+        dp[i+1][0] = i+1;
+    for(int i = 0; i < word2.size(); ++ i)
+        dp[0][i+1] = i+1;
+    
+    for(int i = 0; i < word1.size(); ++ i){
+        for(int j = 0; j < word2.size(); ++ j){
+            if(word1[i] == word2[j]){
+                dp[i+1][j+1] = dp[i][j];
+            }
+            else{
+                dp[i+1][j+1] = min(dp[i][j], min(dp[i][j+1], dp[i+1][j])) + 1;
+            }
+        }
+    }
+    return dp.back().back();
+}
+
 int main(int argc, char* argv[]) {
     string lineToAdd;
     string firstLine;
